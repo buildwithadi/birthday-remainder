3dbf5557a2fc0555c6146c03b60b9cc9ef768462
buildwithadi <adityarawat.online@gmail.com>
2025-08-20 21:13:46 +0530

    day-1-tasks-completed

--- remainder/admin.py
+++ remainder/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User, Contact, EmailReminder
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name','date_of_birth', 'user',)
+    search_fields = ('name',)
+
+@admin.register(EmailReminder)
+class EmailReminderAdmin(admin.ModelAdmin):
+    list_display = ('contact', 'sent_at')
+    list_filter = ('sent_at',)
--- remainder/models.py
+++ remainder/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+NOTIFICATION_PREFERENCE_CHOICES = [
+    ('same_day', 'Same day'),
+    ('one_day_before', 'One day before'),
+    ('two_days_before', 'Two days before'),
+]
 
 # Create your models here.
+class User(AbstractUser):
+    notification_preference = models.CharField(
+        max_length=50,
+        choices=NOTIFICATION_PREFERENCE_CHOICES,
+        default='same_day', # Set a default value
+        verbose_name="Notification Preference"
+    )
+
+    def __str__(self):
+        return self.username
+    
+class Contact(models.Model):
+    user = models.ForeignKey(
+        to = settings.AUTH_USER_MODEL,  # connected to
+        on_delete = models.CASCADE,     # to avoid orphan rows
+        related_name='contacts',        # related table(shortname)
+    )
+    name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+
+    def __str__(self):
+        return f"{self.name} for {self.user.username}"
+    
+class EmailReminder(models.Model):
+    contact = models.ForeignKey(
+        to = 'Contact',             # connected to
+        on_delete = models.CASCADE, # to avoid orphan rows
+        related_name = 'reminders'  # related table(shortname)
+    )
+
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Remainder for {self.contact.name}"
